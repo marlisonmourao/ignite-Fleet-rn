1d2af5634a53b6d16d3e472aa79d5cc3647224d2
marlisonmourao <bentesmourao@gmail.com>
2023-05-23 21:39:56 -0400

    fecting data vehicle

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,52 @@
+import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { CarStatus } from '@components/CarStatus'
+import { useQuery } from '@libs/realm'
+
 import { Container, Content } from './styles'
 
+import { CarStatus } from '@components/CarStatus'
 import { Header } from '@components/Header'
 
+import { Historic } from '@libs/realm/schemas/Historic'
+import { Alert } from 'react-native'
+
 export function Home() {
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null)
+
   const navigation = useNavigation()
+
+  const historic = useQuery(Historic)
 
   function handleRegisterMovement() {
     navigation.navigate('departure')
   }
+
+  function handleFechVehicle() {
+    try {
+      const vehicle = historic.filtered("status = 'departure'")[0]
+      setVehicleInUse(vehicle)
+    } catch (error) {
+      console.log(error)
+      Alert.alert(
+        'Veículo em uso',
+        'Não foi possível carregar o veículo em uso.',
+      )
+    }
+  }
+
+  useEffect(() => {
+    handleFechVehicle()
+  }, [])
 
   return (
     <Container>
       <Header />
 
       <Content>
-        <CarStatus onPress={handleRegisterMovement} />
+        <CarStatus
+          onPress={handleRegisterMovement}
+          licensePlate={vehicleInUse?.license_plate}
+        />
       </Content>
     </Container>
   )
