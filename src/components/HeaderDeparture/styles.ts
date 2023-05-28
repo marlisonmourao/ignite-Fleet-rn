fa944bf7cc0ab8a3401b94a6ccc3f1a00edb6869
marlisonmourao <bentesmourao@gmail.com>
2023-05-27 21:33:39 -0400

    delete/z-index

--- src/components/HeaderDeparture/styles.ts
+++ src/components/HeaderDeparture/styles.ts
@@ -7,7 +7,6 @@ export const Container = styled.View`
   justify-content: space-between;
 
   background-color: ${({ theme }) => theme.colors.GRAY_700};
-  z-index: 1;
 `
 
 export const Title = styled.Text`
