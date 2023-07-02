a9677a3ed77ae6380b4983ea0fb1d6a356d79e63
marlisonmourao <bentesmourao@gmail.com>
2023-07-02 15:49:53 -0400

    type .env

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -2,4 +2,5 @@ declare module '@env' {
   export const ANDROID_CLIENT_ID: string
   export const IOS_CLIENT_ID: string
   export const REAL_APP_ID: string
+  export const GOOOGLE_MAPS_API_KEY: string
 }
