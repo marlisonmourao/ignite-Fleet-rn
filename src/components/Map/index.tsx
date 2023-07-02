0a7de9978aa36b482269e46d7b7e77f2d08d22f9
marlisonmourao <bentesmourao@gmail.com>
2023-07-02 16:59:21 -0400

    showing line between points on map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,14 @@ import MapView, {
   PROVIDER_GOOGLE,
   MapViewProps,
   LatLng,
+  Marker,
+  Polyline,
 } from 'react-native-maps'
+import { useTheme } from 'styled-components'
+
+import { IconBox } from '@components/IconBox'
+import { Car, FlagCheckered } from 'phosphor-react-native'
+import { useRef } from 'react'
 
 type Props = MapViewProps & {
   coordinates: LatLng[]
@@ -11,8 +18,20 @@ type Props = MapViewProps & {
 export function Map({ coordinates, ...rest }: Props) {
   const lastCoordinate = coordinates[coordinates.length - 1]
 
+  const { colors } = useTheme()
+  const mapRef = useRef<MapView>(null)
+
+  async function onMapLoaded() {
+    if (coordinates.length > 1) {
+      mapRef.current?.fitToSuppliedMarkers(['departure', 'arrival'], {
+        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+      })
+    }
+  }
+
   return (
     <MapView
+      ref={mapRef}
       provider={PROVIDER_GOOGLE}
       style={{ width: '100%', height: 200 }}
       region={{
@@ -21,7 +40,26 @@ export function Map({ coordinates, ...rest }: Props) {
         latitudeDelta: 0.005,
         longitudeDelta: 0.005,
       }}
+      onMapLoaded={onMapLoaded}
       {...rest}
-    />
+    >
+      <Marker identifier="departure" coordinate={coordinates[0]}>
+        <IconBox icon={Car} size="SMALL" />
+      </Marker>
+
+      {coordinates.length > 1 && (
+        <>
+          <Marker identifier="arrival" coordinate={lastCoordinate}>
+            <IconBox icon={FlagCheckered} size="SMALL" />
+          </Marker>
+
+          <Polyline
+            coordinates={[...coordinates]}
+            strokeColor={colors.GRAY_700}
+            strokeWidth={7}
+          />
+        </>
+      )}
+    </MapView>
   )
 }
