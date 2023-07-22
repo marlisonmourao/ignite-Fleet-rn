b434c757472da0feceff1051af4a6d08c6aef4cc
marlisonmourao <bentesmourao@gmail.com>
2023-07-22 15:01:49 -0400

    feat: background task location

--- src/tasks/backgroundTaskLocation.ts
+++ src/tasks/backgroundTaskLocation.ts
@@ -1,4 +1,5 @@
 import * as TaskManager from 'expo-task-manager'
+import { startLocationUpdatesAsync, Accuracy } from 'expo-location'
 
 export const BACKGROUND_TASK_NAME = 'location-tracking'
 
@@ -21,3 +22,15 @@ TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
     console.log(error)
   }
 })
+
+export async function startLocationTask() {
+  try {
+    await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
+      accuracy: Accuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
